153ca7660534c202653d2e4e4973c7191ffbc70d
sadhu-sanjay <sanjaygoswami60@gmail.com>
2024-11-27 13:17:12 +0530

    Here

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+
+export const break1 = 7;
+export const break2 = 14;
+export const break3 = 21;
+export const break4 = 28;
+export const break5 = 35;
+export const break6 = 42;
+export const break7 = 49;
+export const break8 = 56;
+export const break9 = 63;
+export const break10 = 70;
+export const break11 = 80;
+export const break12 = 90;
+export const break13 = 100;
