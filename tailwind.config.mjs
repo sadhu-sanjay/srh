d82bda4cccf4c9fda36f6ede93ed997d86859590
sadhu-sanjay <sanjaygoswami60@gmail.com>
2024-11-30 15:55:22 +0530

    sending it

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,4 @@
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -10,7 +11,15 @@ export default {
 			},
 			fontSize: {
 				primaryText: "33dvw"
-			}	
+			},
+			animation: {
+				"spin": "spin 60s linear infinite"
+			},
+			keyframes: {
+				"spin": {
+					transform: "rotate(360deg)"
+				}
+			}
 		},
 	},
 	plugins: [],
