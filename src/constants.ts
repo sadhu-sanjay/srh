b8d80df8fe8c769a18a1d0d9107185e185a2a244
sadhu-sanjay <sanjaygoswami60@gmail.com>
2024-12-04 18:35:01 +0530

    consissten fo the page

--- src/constants.ts
+++ src/constants.ts
@@ -14,19 +14,20 @@ export const break10 = multipler * 10
 export const break11 = multipler * 11
 export const break12 = multipler * 12
 export const break13 = multipler * 13
+export const break14 = multipler * 14
 
 export const circlesData = [ 
-      { cx: -20, cy: -40, cx1: 120, cy1: 130, cx2: 20, cy2: 0, r: 25, fill: "rgba(0,250,250, 0.9)", fill1: "rgba(0,250,250, 0.9)", },
-      { cx: -20, cy: -40, cx1: 180, cy1: 130, cx2: 20, cy2: 15, r: 25, fill: "rgba(0,250, 250 , 0.9)", fill1: "rgba(0,250, 250 , 0.9)", },
-      { cx: -20, cy: -40, cx1: 120, cy1: 10, cx2: 20, cy2: 30, r: 25, fill: "rgba(0, 250, 0, 0.9)", fill1: "rgba(0, 250, 0, 0.9)", },
-      { cx: -20, cy: -40, cx1: 180, cy1: 10, cx2: 20, cy2: 45, r: 25, fill: "rgba( 0, 250, 0  , 0.9)", fill1: "rgba( 0, 250, 0  , 0.9)", },
-      { cx: -20, cy: -40, cx1: 0, cy1: 70, cx2: 20, cy2: 60, r: 25, fill: "rgba( 250,250,0, 0.9)", fill1: "rgba( 250,250,0, 0.9)", },
-      { cx: -20, cy: -40, cx1: 60, cy1: 70, cx2: 20, cy2: 75, r: 25, fill: "rgba( var(--accent), 0.3)", fill1: "rgba(var(--accent), 0.9)", },
-      { cx: 120, cy: 90, cx1: 0, cy1: 130, cx2: 20, cy2: 90, r: 100, fill: "rgba( var(--accent), 0.4)", fill1: "rgba(var(--accent), 0.9)", },
-      { cx: 100, cy: 90, cx1: 60, cy1: 130, cx2: 20, cy2: 105, r: 85, fill: "rgba( var(--accent), 0.5)", fill1: "rgba(var(--accent), 0.9)", },
-      { cx: 120, cy: 80, cx1: 0, cy1: 10, cx2: 20, cy2: 120, r: 70, fill: "rgba(var(--accent), 0.4)", fill1: "rgba(var(--accent), 0.9)", },
-      { cx: 80, cy: 80, cx1: 60, cy1: 10, cx2: 20, cy2: 135, r: 55, fill: "rgba(var(--accent), 0.4)", fill1: "rgba(var(--accent), 0.9)", },
-      { cx: 130, cy: 100, cx1: 180, cy1: 70, cx2: 20, cy2: 150, r: 40, fill: "rgba(var(--accent), 0.4)", fill1: "rgba(var(--accent), 0.9)", },
-      { cx: 130, cy: 80, cx1: 120, cy1: 70, cx2: 20, cy2: 165, r: 25, fill: "rgba(var(--accent), 0.4)", fill1: "rgba(var(--accent), 0.9)", },
+      { cx: -20, cy: -40, cx1: 120, cy1: 130, cx2: 15, cy2: 0, r: 25, fill: "rgba(0,250,250, 0.9)", fill1: "rgba(0,250,250, 0.9)", },
+      { cx: -20, cy: -40, cx1: 180, cy1: 130, cx2: 15, cy2: 15, r: 25, fill: "rgba(0,250, 250 , 0.9)", fill1: "rgba(0,250, 250 , 0.9)", },
+      { cx: -20, cy: -40, cx1: 120, cy1: 10, cx2: 15, cy2: 30, r: 25, fill: "rgba(0, 250, 0, 0.9)", fill1: "rgba(0, 250, 0, 0.9)", },
+      { cx: -20, cy: -40, cx1: 180, cy1: 10, cx2: 15, cy2: 45, r: 25, fill: "rgba( 0, 250, 0  , 0.9)", fill1: "rgba( 0, 250, 0  , 0.9)", },
+      { cx: -20, cy: -40, cx1: 0, cy1: 70, cx2: 15, cy2: 60, r: 25, fill: "rgba( 250,250,0, 0.9)", fill1: "rgba( 250,250,0, 0.9)", },
+      { cx: -20, cy: -40, cx1: 60, cy1: 70, cx2: 15, cy2: 75, r: 25, fill: "rgba( var(--accent), 0.3)", fill1: "rgba(var(--accent), 0.9)", },
+      { cx: 120, cy: 90, cx1: 0, cy1: 130, cx2: 15, cy2: 90, r: 100, fill: "rgba( var(--accent), 0.4)", fill1: "rgba(var(--accent), 0.9)", },
+      { cx: 100, cy: 90, cx1: 60, cy1: 130, cx2: 15, cy2: 105, r: 85, fill: "rgba( var(--accent), 0.5)", fill1: "rgba(var(--accent), 0.9)", },
+      { cx: 120, cy: 80, cx1: 0, cy1: 10, cx2: 15, cy2: 120, r: 70, fill: "rgba(var(--accent), 0.4)", fill1: "rgba(var(--accent), 0.9)", },
+      { cx: 80, cy: 80, cx1: 60, cy1: 10, cx2: 15, cy2: 135, r: 55, fill: "rgba(var(--accent), 0.4)", fill1: "rgba(var(--accent), 0.9)", },
+      { cx: 130, cy: 100, cx1: 180, cy1: 70, cx2: 15, cy2: 150, r: 40, fill: "rgba(var(--accent), 0.4)", fill1: "rgba(var(--accent), 0.9)", },
+      { cx: 130, cy: 80, cx1: 120, cy1: 70, cx2: 15, cy2: 165, r: 25, fill: "rgba(var(--accent), 0.4)", fill1: "rgba(var(--accent), 0.9)", },
 ];
 
